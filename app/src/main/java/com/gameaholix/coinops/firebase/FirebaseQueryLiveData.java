41c84358d34246a3bdcfa6f5325cb4b56b807ebe
mkillewald <mkillewald@yahoo.com>
2018-11-28 14:05:08 -0500

    added 2 second delay before removing the database listener.
     * for more information, see https://firebase.googleblog.com/2017/12/using-android-architecture-components_22.html

--- app/src/main/java/com/gameaholix/coinops/firebase/FirebaseQueryLiveData.java
+++ app/src/main/java/com/gameaholix/coinops/firebase/FirebaseQueryLiveData.java
@@ -1,6 +1,8 @@
 package com.gameaholix.coinops.firebase;
 
 import android.arch.lifecycle.LiveData;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
@@ -11,11 +13,21 @@ import com.google.firebase.database.ValueEventListener;
 
 // Concepts and code used from 3 part series:
 // https://firebase.googleblog.com/2017/12/using-android-architecture-components.html
+
 public class FirebaseQueryLiveData extends LiveData<DataSnapshot> {
     private static final String TAG = FirebaseQueryLiveData.class.getSimpleName();
 
+    private boolean listenerRemovePending = false;
     private final Query mQuery;
     private final MyValueEventListener mListener = new MyValueEventListener();
+    private final Handler handler = new Handler();
+    private final Runnable removeListener = new Runnable() {
+        @Override
+        public void run() {
+            mQuery.removeEventListener(mListener);
+            listenerRemovePending = false;
+        }
+    };
 
     public FirebaseQueryLiveData(Query query) {
         this.mQuery = query;
@@ -27,24 +39,32 @@ public class FirebaseQueryLiveData extends LiveData<DataSnapshot> {
 
     @Override
     protected void onActive() {
-        Log.d(TAG, "onActive");
-        mQuery.addValueEventListener(mListener);
+        if (listenerRemovePending) {
+            handler.removeCallbacks(removeListener);
+        }
+        else {
+            mQuery.addValueEventListener(mListener);
+        }
+        listenerRemovePending = false;
     }
 
     @Override
     protected void onInactive() {
-        Log.d(TAG, "onInactive");
-        mQuery.removeEventListener(mListener);
+        // Listener removal is schedule on a two second delay to avoid listner removal and
+        // re-add during configuration changes (device rotation) since that would trigger an unnecessary
+        // download of data from the database.
+        handler.postDelayed(removeListener, 2000);
+        listenerRemovePending = true;
     }
 
     private class MyValueEventListener implements ValueEventListener {
         @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
             setValue(dataSnapshot);
         }
 
         @Override
-        public void onCancelled(DatabaseError databaseError) {
+        public void onCancelled(@NonNull DatabaseError databaseError) {
             Log.e(TAG, "Can't listen to query " + mQuery, databaseError.toException());
         }
     }
